c3c79b45f657fda639c33b628953ab69fd2cfd15
Otto <yashar.man82@gmail.com>
2025-12-11 21:46:12 +0330

    Added Boolen, Condition and Function Sections

--- Files/Scripts/jsll.js
+++ Files/Scripts/jsll.js
@@ -18,11 +18,69 @@ let cartQuantity = 0; //modern var
 const MESSAGE = "Hello"; //constant
 var variable = 5; //old
 
-console.log(typeof variable);
-console.log(typeof MESSAGE);
+// console.log(typeof variable);
+// console.log(typeof MESSAGE);
 
 // Calculator Lab
 
 let calculation = '';
+
+// Booleans Lab
+
+// console.log(5 === 5.00);
+// console.log(5 === "5.00");
+
+// if (calculation) {
+//     console.log(calculation);
+// }
+// else if (calculation === '0') {
+//     console.log(calculation)
+// }
+// else {
+//     console.log("Enter Instruction");
+// }
+
+let randomNumber = 0;
+let computerMove = '';
+let result = '';
+
+// Falsy Values: false 0 '' NaN undefined null
+
+// Short Condision: Condision ? Truethy : Falsy
+// Guard Operator: Condision && Instruction
+// let message;
+// if (condition) {
+//     message = 'Hello';
+// }
+// Shortcut >> let message = condition && 'Hello
+
+// const currency = 'EUR' || 'USD';
+// console.log(currency) --> EUR
+
+// Functions Lab
+
+// function func1() {
+//     console.log('Hello from func1');
+//     return 0;
+// }
+
+function pickComputerMove() {
+    randomNumber = Math.random();
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'Rock';
+    }
+    else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'Paper';
+    }
+    else {
+        computerMove = 'Scissors';
+    }
+
+    return computerMove;
+}
+
+// console.log(result = func1());
+
 
 // End of website-v2.js
