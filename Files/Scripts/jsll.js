cf119548e5ba2be42fc41279b49f9dbad63dd414
Otto <yashar.man82@gmail.com>
2025-12-11 18:50:48 +0330

    Added Calculator

--- Files/Scripts/jsll.js
+++ Files/Scripts/jsll.js
@@ -2,12 +2,13 @@
 // JavaScript code for Website-v2
 
 // This code performs basic arithmetic operations and logs the results to the console
-let calculation = 5 + 3;
-console.log(`The result of 5 + 3 is: ${calculation}`);
-calculation = calculation * 2;
-console.log(`After multiplying by 2, the result is: ${calculation}`);
-calculation = calculation - 4;
-console.log(`After subtracting 4, the final result is: ${calculation}`);
+// let calculation = 5 + 3;
+// console.log(`The result of 5 + 3 is: ${calculation}`);
+// calculation = calculation * 2;
+// console.log(`After multiplying by 2, the result is: ${calculation}`);
+// calculation = calculation - 4;
+// console.log(`After subtracting 4, the final result is: ${calculation}`);
+
 // End of arithmetic operations
 
 // Buttons Lab
@@ -19,5 +20,9 @@ var variable = 5; //old
 
 console.log(typeof variable);
 console.log(typeof MESSAGE);
+
+// Calculator Lab
+
+let calculation = '';
 
 // End of website-v2.js
